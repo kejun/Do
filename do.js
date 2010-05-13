046478421f06058bbaa9d2780cad91d072f14deb
kejun <Kejun@Kejuns-MacBook-Pro.local>
2010-05-13 12:05:05 +0800

    add comments & test file

--- do.js
+++ do.js
@@ -1,17 +1,22 @@
 /**
- * Do 是一个轻量级javascript开发框架。它的核心库可以自由更换。
- * 设计原则：
- * 1. 不提供任何业务相关功能
- * 2. 公共功能功能都出自核心库，核心库可灵活添加或更换。
- * 3. 内置依赖关系管理系统
+ * Do 是一个轻量级javascript开发框架。
+ * Do设计原则上不提供与具体的业务逻辑相关的功能。
+ * 主要解决模块的依赖关系和异步加载。保持足够轻量。
+ * 它的核心库可以自由更换。目前是jQuery，也可以多个。
  * @author Kejun (listenpro@gmail.com)
- * @version 0.1(draft)
+ * @version 0.1.1
  */
 
 (function(){
 
 var _Doc = document, 
+
+// 已加载模块, _loaded[file_url]=true
 _loaded = {},
+
+// 加载中的模块，对付慢文件，_loading_queue[url]=true|false
+_loading_queue = {},
+
 _isArray = function (e) { return e.constructor === Array; },
 
 _log = function (e) {
@@ -21,20 +26,20 @@ _log = function (e) {
 },
 
 
-//内部配置文件
+// 内部配置文件
 _config = {
-    //核心库，可以任意换
+    //核心库
     core_lib: ['http://t.douban.com/js/jquery1.4.2.min.js'],
     
     //模块依赖
+    //{ moduleName: {path: 'URL', type:'js|css', requires:['moduleName1', 'fileURL']} }
     mods: {}
 },
 
-_loading_queue = {},
-
+// 插入的参考结点
 _file = _Doc.getElementsByTagName('script')[0],
 
-// load external js or css.
+// 加载js/css文件 
 _load = function (url, type, charset, cb) {
     if (!url) {
         return;
@@ -48,12 +53,15 @@ _load = function (url, type, charset, cb) {
         return;
     }
     
+    // 加载中的文件有可能是太大，有可能是404
+    // 当加载队列中再次出现此模块会再次加载，理论上会出现重复加载
     if (_loading_queue[url]) {
         setTimeout(function(){
             _load(url, type, charset, cb);
         }, 10);
         return;
     }
+
     _loading_queue[url] = true;
     
     var n, t = type || url.toLowerCase().substring(url.lastIndexOf('.') + 1);
@@ -62,6 +70,7 @@ _load = function (url, type, charset, cb) {
         n = _Doc.createElement('script');
         n.setAttribute('type', 'text/javascript');
         n.setAttribute('src', url);
+        n.setAttribute('async', true);
     } else if (t === 'css') {
         n = _Doc.createElement('link');
         n.setAttribute('type', 'text/css');
@@ -74,6 +83,7 @@ _load = function (url, type, charset, cb) {
         n.charset = charset;
     }
     
+    // CSS无必要监听是否加载完毕
     if (t === 'css') {
       _file.parentNode.insertBefore(n, _file);
       if (cb) {
@@ -84,7 +94,7 @@ _load = function (url, type, charset, cb) {
     
     n.onload = n.onreadystatechange = function () {
         if (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete') {
-            //loaded success.
+
             _loaded[this.getAttribute('src')] = true;
             
             if (cb) {
@@ -98,24 +108,26 @@ _load = function (url, type, charset, cb) {
     _file.parentNode.insertBefore(n, _file);
 },
 
+// 计算加载队列。参数e是一个数组
 _calculate = function(e) {
     if (!e || !_isArray(e) ) {
         return;
     }
     
-    var i = 0, item, result = [], 
+    var i = 0, 
+    item, 
+    result = [], 
     mods = _config.mods,
-    _depeList = [],
-    _hasAdded = {},
+    depeList = [],
+    hasAdded = {},
     getDepeList = function (e) {
-
         var i = 0, m, reqs;
         
         // break loop require.
-        if (_hasAdded[e]) {
-            return _depeList;
+        if (hasAdded[e]) {
+            return depeList;
         }   
-        _hasAdded[e] = true;
+        hasAdded[e] = true;
         
         if (mods[e].requires) {
             reqs = mods[e].requires;
@@ -123,21 +135,21 @@ _calculate = function(e) {
               // is a module.
               if (mods[m]) {
                 getDepeList(m);
-                _depeList.push(m);
+                depeList.push(m);
                } else {
                 // is a file.
-                _depeList.push(m);
+                depeList.push(m);
                }
             }
-            return _depeList;
+            return depeList;
         }
-        return _depeList;
+        return depeList;
     };
     
     for (; item = e[i++]; ) {
         if (mods[item] && mods[item].requires && mods[item].requires[0]) {
-            _depeList = [];
-            _hasAdded = {};
+            depeList = [];
+            hasAdded = {};
             result = result.concat(getDepeList(item));
         }
         result.push(item);
@@ -146,18 +158,23 @@ _calculate = function(e) {
     return result;
 },
 
-// a asynchronous queue
+// 一个异步队列对象 
 _queue = function (e) {
     if (!e || !_isArray(e) ) {
         return;
     }
     
     this.queue = e;
-    //timeout file collection.
+
+    // 超时文件收集
     this._skip = {};
+
+    // 队列当前要加载的模块
+    this.current = null;
 };
 
 _queue.prototype = {
+
     _Timeout: 6000,
     
     _interval: 10,
@@ -171,7 +188,8 @@ _queue.prototype = {
             return;
         }
         
-        
+        // 文件可能超时，加载前启动一个计时器
+        // 超时文件目前处理是踢出队列，这会引起后面有依懒关系的程序出错。所以会将超时文件输出到console里
         this._outTimer = setTimeout(function () {
           _log('[DoubanJS] "' + o.current + '" timeout.');
           o._skip[o.current] = true; 
@@ -182,16 +200,16 @@ _queue.prototype = {
     },
     
     run: function () {
-        
         var o = this, mod;
         
         if (typeof this.current === 'function') {
             this.clearTimer();
+            // 立即执行此Function
             this.current();
             this.start();
+            return;
         } else if (typeof this.current === 'string') {
             if (_config.mods[this.current]) {
-              // todo:load a module.
               mod = _config.mods[this.current];
               _load(mod.path, mod.type, mod.charset, function (e) {
                  // if timeout file fire callback don't disturb queue.
@@ -225,7 +243,7 @@ _queue.prototype = {
 };
 
 
-// preload core lib.
+// 预加载核心库.
 _load(_config.core_lib[0], 'js');
 
 
@@ -239,7 +257,6 @@ this.Do.add = function(sName, oConfig) {
     if (!sName || !oConfig || !oConfig.path) {
         return;
     }
-    
     _config.mods[sName] = oConfig;
 };
 
